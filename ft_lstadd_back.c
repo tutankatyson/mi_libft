7e92ba3bd49be301b0aabcf172e01a364bf9c56e
Jorge Sanchez Vergara <jorsanch@c2r1s2.42malaga.com>
2022-09-30 00:26:49 +0200

    Libft Soft Paco 100%

---
+++ ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorsanch <jorsanch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 12:48:35 by jorsanch          #+#    #+#             */
+/*   Updated: 2022/09/29 20:58:31 by jorsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst != NULL)
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+	else
+		*lst = new;
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorsanch <jorsanch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 15:54:34 by jorsanch          #+#    #+#             */
+/*   Updated: 2022/09/28 21:48:38 by jorsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char)
+			*(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1));
+	if (str)
+		ft_stpcpy(ft_stpcpy(str, s1), s2);
+	return (str);
+}
